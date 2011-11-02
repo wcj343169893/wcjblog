b05a39c20635573dc8a001a92707b7a4541998fc
wcj343169893@163.com <wcj343169893@163.com@698fb5e5-2101-e3ce-9d1d-683b5f06a159>
2011-11-02 09:32:49 +0000

    修改增加文章分类时，上级编号转换问题

--- blog/src/com/google/choujone/blog/servlet/FileDownloadServlet.java
+++ blog/src/com/google/choujone/blog/servlet/FileDownloadServlet.java
@@ -1,6 +1,7 @@
 package com.google.choujone.blog.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,25 +24,35 @@ public class FileDownloadServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.setContentType("image/jpeg");
-		String url = req.getRequestURI();
-		int ind = url.lastIndexOf("/");
-		if (ind == -1)
-			return;
+		try {
+			resp.setContentType("image/jpeg");
+			String url = req.getRequestURI();
+			int ind = url.lastIndexOf("/");
+			if (ind == -1)
+				return;
 
-		String temp[] = url.split("/");
-		String filename = temp[temp.length - 1];
-		ind = filename.indexOf("_");
-		String id = filename.substring(0, ind);
-		filename = filename.substring(ind + 1);
+			String temp[] = url.split("/");
+			String filename = temp[temp.length - 1];
+			ind = filename.indexOf("_");
+			String id = filename.substring(0, ind);
+			filename = filename.substring(ind + 1);
 
-		DataFile df = null;
-		if (id != null) {
-			df = service.get(Long.valueOf(id), true);
-		}
+			DataFile df = null;
+			if (id != null) {
+				df = service.get(Long.valueOf(id), true);
+			}
 
-		if (df != null && df.getFilename().equals(filename)) {
-			resp.getOutputStream().write(df.getFileData().getBytes());
+			if (df != null && df.getFilename().equals(filename)) {
+				resp.getOutputStream().write(df.getFileData().getBytes());
+			}
+		} catch (Exception e) {
+//			resp.setContentType("text/html;charset=utf-8");
+//			resp.setCharacterEncoding("UTF-8");
+//			resp.setHeader("Cache-Control", "no-cache");
+//			PrintWriter out = resp.getWriter();
+//			out.println("/images/error.gif");
+//			out.close();
+			resp.sendRedirect("/images/error.gif");
 		}
 	}
 }
