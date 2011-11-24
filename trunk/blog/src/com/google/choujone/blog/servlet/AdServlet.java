4b277084ecc04ed650e9025c6fccbb7541c253ef
wcj343169893@163.com <wcj343169893@163.com@698fb5e5-2101-e3ce-9d1d-683b5f06a159>
2011-11-24 09:09:59 +0000

    添加广告和广告位概念，需要完善后台和前台

---
+++ trunk/blog/src/com/google/choujone/blog/servlet/AdServlet.java
@@ -0,0 +1,45 @@
+package com.google.choujone.blog.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// 前台显示广告
+		resp.setContentType("text/html;charset=utf-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setHeader("Cache-Control", "no-cache");
+		PrintWriter out = resp.getWriter();
+		String code = "";
+		try {
+
+			String url = req.getRequestURI();
+			int ind = url.lastIndexOf("/");
+			if (ind == -1)
+				return;
+
+			String temp[] = url.split("/");
+			String id = temp[temp.length - 1];//获取广告位编号
+			//相同广告位的广告，随机抓取
+			code = "广告位:" + id;
+		} catch (Exception e) {
+		}
+		out.write(code);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// 后台管理广告
+		//添加广告位
+		
+		//添加广告
+	}
+}
