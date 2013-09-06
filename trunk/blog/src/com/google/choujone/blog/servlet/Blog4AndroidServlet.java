796645a0a528e65accd8d96233519d806ef35abc
wcj343169893@gmail.com <wcj343169893@gmail.com@698fb5e5-2101-e3ce-9d1d-683b5f06a159>
2013-09-06 05:34:33 +0000

    修改bug

--- trunk/blog/src/com/google/choujone/blog/servlet/Blog4AndroidServlet.java
+++ trunk/blog/src/com/google/choujone/blog/servlet/Blog4AndroidServlet.java
@@ -21,27 +21,28 @@ public class Blog4AndroidServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		boolean flag = false;
-		String title = req.getParameter("title");// 标题
-		String content = req.getParameter("content");// 文章信息
-		String tag = req.getParameter("tag");// 关键字
-		String isVisible = req.getParameter("isVisible") != null ? req
-				.getParameter("isVisible") : "1";// 是否发布
-		String tid = req.getParameter("tid") != null ? req.getParameter("tid")
-				: "-1L";// 分类id
-		Blog blog = new Blog();
-		BlogDao blogDao = new BlogDao();
-		blog.setTag(tag);
-		blog.setTitle(title);
-		blog.setTid(Long.valueOf(tid));
-		blog.setIsVisible(Integer.parseInt(isVisible));
-		blog.setContent(new com.google.appengine.api.datastore.Text(Tools
-				.changeHTML(Tools.toChinese(content))));
-		blog.setCount(0);
-		blog.setReplyCount(0);
-		blog.setSdTime(Tools.changeTime(new Date()));
-		blog.setSource("android发布");
-		flag = blogDao.operationBlog(Operation.add, blog);
-
+		if (Tools.isLogin(req)) {
+			String title = req.getParameter("title");// 标题
+			String content = req.getParameter("content");// 文章信息
+			String tag = req.getParameter("tag");// 关键字
+			String isVisible = req.getParameter("isVisible") != null ? req
+					.getParameter("isVisible") : "1";// 是否发布
+			String tid = req.getParameter("tid") != null ? req
+					.getParameter("tid") : "-1L";// 分类id
+			Blog blog = new Blog();
+			BlogDao blogDao = new BlogDao();
+			blog.setTag(tag);
+			blog.setTitle(title);
+			blog.setTid(Long.valueOf(tid));
+			blog.setIsVisible(Integer.parseInt(isVisible));
+			blog.setContent(new com.google.appengine.api.datastore.Text(Tools
+					.changeHTML(Tools.toChinese(content))));
+			blog.setCount(0);
+			blog.setReplyCount(0);
+			blog.setSdTime(Tools.changeTime(new Date()));
+			blog.setSource("android发布");
+			flag = blogDao.operationBlog(Operation.add, blog);
+		}
 		resp.setContentType("text/html;charset=utf-8");
 		resp.setCharacterEncoding("UTF-8");
 		resp.setHeader("Cache-Control", "no-cache");
